4edd321794351b50aded84ad97660a3c284b3145
Giorgio Comai <g@giorgiocomai.eu>
2021-07-27 23:32:22 +0200

    introduce drop_na option in cas_count

--- R/cas_count.R
+++ R/cas_count.R
@@ -5,6 +5,7 @@
 #' @param text Defaults to text. The unquoted name of the column of the corpus data frame to be used for matching.
 #' @param group_by Defaults to NULL. If given, the unquoted name of the column to be used for grouping (e.g. date, or doc_id, or source, etc.)
 #' @param ignore_case Defaults to TRUE.
+#' @param drop_na Defaults to TRUE. If TRUE, all rows where either date or text is NA are removed before further processing.
 #' @param full_words_only Defaults to FALSE. If FALSE, string is counted even when the it is found in the middle of a word (e.g. if FALSE, "ratio" would be counted as match in the word "irrational").
 #' @param locale Locale to be used when ignore_case is set to TRUE. Passed to `stringr::str_to_lower`, defaults to "en".
 #' @param n_column_name Defaults to 'n'. The unquoted name of the column to be used for the count in the output.
@@ -30,11 +31,18 @@ cas_count <- function(corpus,
                       text = text,
                       group_by = date,
                       ignore_case = TRUE,
+                      drop_na = TRUE,
                       fixed = FALSE,
                       full_words_only = FALSE,
                       string_column_name = string,
                       n_column_name = n,
                       locale = "en") {
+
+  if (drop_na==TRUE) {
+    corpus <- corpus %>%
+      dplyr::filter(is.na({{ text }})==FALSE, is.na({{ group_by }})==FALSE)
+  }
+
   if (isTRUE(full_words_only)) {
     string <- purrr::map_chr(.x = string,
                             .f = function(x) {
@@ -104,6 +112,20 @@ cas_count_single <- function(corpus,
 }
 
 
+#' Count total words in a dataset
+#'
+#' @param corpus A textual corpus as a data frame.
+#' @param string
+#' @param text
+#' @param group_by
+#' @param ignore_case
+#' @param n_column_name
+#' @param locale
+#'
+#' @return
+#' @export
+#'
+#' @examples
 cas_count_total_words <-  function(corpus,
                                    string = "[\\w\']+",
                                    text = text,
@@ -113,7 +135,7 @@ cas_count_total_words <-  function(corpus,
                                    locale = "en") {
   cas_count_single(corpus = corpus,
                    string = string,
-                   text ={{ text }},
+                   text = {{ text }},
                    group_by = {{ group_by }},
                    ignore_case = FALSE,
                    fixed = FALSE,
