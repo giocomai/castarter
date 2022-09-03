eb7f0929fa954087d0391715b5bf1fd2c68e8d9b
Giorgio Comai <g@giorgiocomai.eu>
2022-09-03 22:24:51 +0200

    fix remaining colname change issues

--- data-raw/casdb_empty_index_id.R
+++ data-raw/casdb_empty_index_id.R
@@ -1,12 +1,12 @@
 ## code to prepare `casdb_empty_index_id` dataset goes here
 
 
-casdb_empty_index_id <- c("index_id", "url", "type")%>%
+casdb_empty_index_id <- c("id", "url", "type")%>%
   purrr::map_dfc(setNames,
                  object = list(character())
   )
 
-casdb_empty_index_id$index_id <- as.numeric(casdb_empty_index_id$index_id)
+casdb_empty_index_id$id <- as.numeric(casdb_empty_index_id$id)
 
 casdb_empty_index_id <- tibble::as_tibble(casdb_empty_index_id)
 
