7b69db183f15034ad9df054ddab54ebbf565afda
Giorgio Comai <g@giorgiocomai.eu>
2023-01-09 00:03:28 +0100

    more options for cas_ia_check

--- R/cas_ia_check.R
+++ R/cas_ia_check.R
@@ -7,7 +7,10 @@
 #' https://github.com/hrbrmstr/wayback
 #'
 #' @param url A charachter vector of length one, a url.
-#'
+#' @param check_db Defaults to TRUE. If TRUE, checks if given URL has already
+#'   been checked in local database, and queries APIs only for URLs that have
+#'   not been previously checked.
+#' @param write_db Defaults to TRUE. If TRUE, writes result to a local database.
 #' @inheritParams cas_download
 #'
 #' @return A url linking to the version on the Internet Archive
@@ -20,60 +23,71 @@
 cas_ia_check <- function(url = NULL,
                          wait = 1,
                          db_connection = NULL,
+                         check_db = TRUE,
+                         write_db = TRUE,
                          ...) {
   if (length(url) < 2) {
     wait <- 0
   }
 
-  db <- cas_connect_to_db(
-    db_connection = db_connection,
-    ...
-  )
-
-  db_result <- tryCatch(
-    cas_read_db_ia(
-      db_connection = db,
-      disconnect_db = FALSE,
-      ...
-    ),
-    error = function(e) {
-      logical(1L)
-    }
-  )
-
-  if (is.null(db_result)) {
-    previous_df <- casdb_empty_ia_check
-  } else if (isFALSE(db_result)) {
-    previous_df <- casdb_empty_ia_check
+  if (check_db == FALSE & write_db == FALSE) {
+    # do nothing, as connection won't be needed
   } else {
-    if (length(db_result %>% dplyr::pull(url)) == 0) {
+    db <- cas_connect_to_db(
+      db_connection = db_connection,
+      ...
+    )
+  }
+
+
+  if (check_db == TRUE) {
+    db_result <- tryCatch(
+      cas_read_db_ia(
+        db_connection = db,
+        disconnect_db = FALSE,
+        ...
+      ),
+      error = function(e) {
+        logical(1L)
+      }
+    )
+
+    if (is.null(db_result)) {
+      previous_df <- casdb_empty_ia_check
+    } else if (isFALSE(db_result)) {
       previous_df <- casdb_empty_ia_check
     } else {
-      previous_df <- db_result
+      if (length(db_result %>% dplyr::pull(url)) == 0) {
+        previous_df <- casdb_empty_ia_check
+      } else {
+        previous_df <- db_result
+      }
     }
-  }
 
-  if (is.null(url)) {
-    url_df <- cas_read_db_contents_id(
-      db_connection = db,
-      disconnect_db = FALSE
-    )
+    if (is.null(url)) {
+      url_df <- cas_read_db_contents_id(
+        db_connection = db,
+        disconnect_db = FALSE
+      )
 
-    url_v <- url_df %>%
-      dplyr::pull(url)
-  } else if (is.data.frame(url)) {
-    url_v <- url_df %>%
-      dplyr::pull(url)
+      url_v <- url_df %>%
+        dplyr::pull(url)
+    } else if (is.data.frame(url)) {
+      url_v <- url_df %>%
+        dplyr::pull(url)
+    } else {
+      url_v <- as.character(url)
+    }
+
+    url_to_process_df <- tibble::tibble(url = unique(url_v)) %>%
+      dplyr::anti_join(
+        y = previous_df,
+        by = "url",
+        copy = TRUE
+      )
   } else {
-    url_v <- as.character(url)
+    url_to_process_df <- tibble::tibble(url = unique(url))
   }
-
-  url_to_process_df <- tibble::tibble(url = unique(url_v)) %>%
-    dplyr::anti_join(
-      y = previous_df,
-      by = "url", 
-      copy = TRUE
-    )
 
   pb <- progress::progress_bar$new(total = nrow(url_to_process_df))
 
@@ -114,25 +128,36 @@ cas_ia_check <- function(url = NULL,
             checked_at = lubridate::as_datetime(Sys.time()),
             url = as.character(x)
           ) %>%
-          dplyr::select("url", dplyr::everything())
+          dplyr::select(
+            "url",
+            dplyr::everything()
+          )
       }
 
-      cas_write_to_db(
-        df = current_df,
-        table = "ia_check",
-        db_connection = db,
-        disconnect_db = FALSE,
-        ...
-      )
+      if (write_db == TRUE) {
+        cas_write_to_db(
+          df = current_df,
+          table = "ia_check",
+          db_connection = db,
+          disconnect_db = FALSE,
+          ...
+        )
+      }
+
 
       current_df
     }
   )
 
-  cas_disconnect_from_db(
-    db_connection = db,
-    ...
-  )
+  if (check_db == FALSE & write_db == FALSE) {
+    # do nothing, as db object won't exist
+  } else {
+    cas_disconnect_from_db(
+      db_connection = db,
+      ...
+    )
+  }
+
 
   output_df
 }
