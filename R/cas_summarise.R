502ef3d5fbd966ae2b215bf99d12149978ae6760
Giorgio Comai <g@giorgiocomai.eu>
2023-03-04 11:45:11 +0100

    inherit directly documentation from slider for ease of access

--- R/cas_summarise.R
+++ R/cas_summarise.R
@@ -6,11 +6,9 @@
 #' @param period Defaults to NULL. A string describing the time unit to be used for summarising. Possible values include "year", "quarter", "month", "day", "hour", "minute", "second", "millisecond".
 #' @param f Defaults to `mean`. Function to be applied over n for all the values in a given time period. Common alterantives would be `mean` or `median`.
 #' @param auto_convert Defaults to FALSE. If FALSE, the date column is returned using the same format as the input; the minimun vale in the given group is used for reference (e.g. all values for January 2022 are summarised as 2021-01-01 it the data were originally given as dates.). If TRUE, it tries to adapt the output to the most intuitive correspondent type; for year, a numeric column with only the year number, for quarter in the format 2022.1, for month in the format 2022-01.
-#' @param every Check `?slider::slide_period()` for details.
-#' @param before Check `?slider::slide_period()` for details.
-#' @param after Check `?slider::slide_period()` for details.
-#' @param complete Check `?slider::slide_period()` for details.
-#'
+#' 
+#' @inheritParams slider::slide_period
+#' 
 #' @return A data frame with two columns: the name of the period, and the same name originally used for `n`.
 #' @export
 #'
