8803d745decaa0e4bcd91c1b5b91f9cde118ae80
Giorgio Comai <g@giorgiocomai.eu>
2023-04-24 09:42:36 +0200

    add meaningful error message when corpus does not exist

--- R/cas_read_corpus.R
+++ R/cas_read_corpus.R
@@ -16,14 +16,21 @@ cas_read_corpus <- function(path = NULL,
                             corpus_folder = "corpus",
                             ...) {
   if (is.null(path) == TRUE) {
-    path <- cas_get_corpus_path(
+    base_path <- cas_get_corpus_path(
       corpus_folder = corpus_folder,
       file_format = file_format,
       partition = partition,
       token = token,
       ...
     ) %>%
-      fs::path_dir() %>%
+      fs::path_dir() 
+    
+    if (fs::dir_exists(base_path)==FALSE) {
+      cli::cli_abort(message = c(x = "No relevant corpus has yet been created; folder {.path {base_path}} does not exist.",
+                                 i = "You can store a corpus with {.code cas_write_corpus()}"))
+    }
+    
+    path <- base_path %>%
       fs::dir_ls(type = "directory") %>%
       tail(1)
   }
