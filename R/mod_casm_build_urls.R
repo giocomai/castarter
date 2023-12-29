e7fc9bfe6a5a3fa833560c6614e1c5e7ee58e42c
Giorgio Comai <g@giorgiocomai.eu>
2023-12-29 19:32:07 +0200

    introduce shinymeta to building urls shiny modules

--- R/mod_casm_build_urls.R
+++ R/mod_casm_build_urls.R
@@ -12,7 +12,8 @@ mod_casm_build_urls_ui <- function(id) {
   shiny::sidebarLayout(
     sidebarPanel = shiny::sidebarPanel(
       shiny::radioButtons(
-        inputId = ns("url_type"), label = "Type of index urls",
+        inputId = ns("url_type"),
+        label = "Type of index urls",
         choices = c(
           "Incremental",
           "Date-based"
@@ -22,7 +23,11 @@ mod_casm_build_urls_ui <- function(id) {
       shiny::uiOutput(outputId = ns("build_urls_parameters_ui"))
     ),
     mainPanel = shiny::mainPanel(
-      reactable::reactableOutput(outputId = ns("urls_table"))
+      reactable::reactableOutput(outputId = ns("urls_table")),
+      shiny::h3("Simplified code:"),
+      shiny::verbatimTextOutput(outputId = ns("function_string")),
+      shiny::h3("Full code:"),
+      shiny::verbatimTextOutput(ns("code"))
     )
   )
 }
@@ -106,6 +111,22 @@ mod_casm_build_urls_server <- function(id) {
                 "year"
               )
             ),
+            shiny::selectizeInput(
+              inputId = ns("date_format"),
+              label = "Date format",
+              choices = c(
+                "Ymd",
+                "dmY",
+                "mdY"
+              ),
+              options = list(create = TRUE)
+            ),
+            shiny::textInput(
+              inputId = ns("date_separator"),
+              label = "Date separator",
+              value = "-",
+              placeholder = "Insert date separator"
+            ),
             shiny::textInput(
               inputId = ns("index_group"),
               label = "Index group",
@@ -117,23 +138,95 @@ mod_casm_build_urls_server <- function(id) {
     })
 
 
-
-
-    urls_df_r <- shiny::reactive({
-      cas_build_urls(
-        url = input$url,
-        url_ending = input$url_ending,
-        start_page = input$start_page,
-        end_page = input$end_page,
-        increase_by = input$increase_by,
-        date_format = input$date_format,
-        increase_date_by = input$increase_date_by
-      )
+    urls_df_r <- shinymeta::metaReactive2({
+      if (input$url_type == "Incremental") {
+        shinymeta::metaExpr(
+          cas_build_urls(
+            url = ..(input$url),
+            url_ending = ..(input$url_ending),
+            start_page = ..(input$start_page),
+            end_page = ..(input$end_page),
+            increase_by = ..(input$increase_by)
+          )
+        )
+      } else {
+        shinymeta::metaExpr(
+          cas_build_urls(
+            url = ..(input$url),
+            url_ending = ..(input$url_ending),
+            increase_by = ..(input$increase_by),
+            start_date = ..(as.character(input$date_range[[1]])),
+            end_date = ..(as.character(input$date_range[[2]])),
+            date_format = ..(input$date_format),
+            date_separator = ..(input$date_separator),
+            increase_date_by = ..(input$increase_date_by)
+          )
+        )
+      }
     })
 
+    output$function_string <- shiny::renderPrint({
+      if (input$url_type == "Incremental") {
+        styler::style_text(
+          stringr::str_c("cas_build_urls(", 
+                         "url = ",
+                         "\"", input$url, "\",",
+                         "\n",
+                         "url_ending = ",
+                         "\"", input$url_ending, "\",",
+                         "\n",
+                         "start_page = ",
+                         input$start_page, ",",
+                         "\n",
+                         "end_page = ",
+                         input$end_page, ",",
+                         "\n",
+                         "increase_by = ",
+                         "\"", input$increase_by, "\"",
+                         "\n",
+                         ")")
+        )  
+      } else {
+        styler::style_text(
+          stringr::str_c("cas_build_urls(", 
+                         "url = ",
+                         "\"", input$url, "\",",
+                         "\n",
+                         "url_ending = ",
+                         "\"", input$url_ending, "\",",
+                         "\n",
+                         "date_format = ",
+                         "\"", input$date_format, "\",",
+                         "\n",
+                         "date_separator = ",
+                         "\"", input$date_separator, "\",",
+                         "\n",
+                         "start_date = ",
+                         "\"", input$date_range[[1]], "\",",
+                         "\n",
+                         "end_date = ",
+                         "\"", input$date_range[[2]], "\",",
+                         "\n",
+                         "increase_date_by = ",
+                         input$increase_date_by,
+                         "\n",
+                         ")")
+        )
+      }
+    })
+    
     output$urls_table <- reactable::renderReactable({
       reactable::reactable(data = urls_df_r())
     })
+    
+    ## shinymeta
+    
+    output$code <- renderPrint({
+      shinymeta::expandChain(
+        urls_df_r()
+      )
+    })
+    
   })
 }
 
@@ -147,11 +240,11 @@ mod_casm_build_urls_server <- function(id) {
 
 mod_casm_build_urls <- function() {
   ui <- shiny::fluidPage(
-    mod_casm_build_urls_ui(id = "casm_build_urls_1")
+    mod_casm_build_urls_ui(id = "index_urls_df")
   )
 
   server <- function(input, output, session) {
-    urls_r <- mod_casm_build_urls_server(id = "casm_build_urls_1")
+    urls_r <- mod_casm_build_urls_server(id = "index_urls_df")
   }
   shiny::shinyApp(ui, server)
 }
