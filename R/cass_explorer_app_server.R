e734208835e8a7866d3a11de4d040c4c22183a13
Giorgio Comai <g@giorgiocomai.eu>
2024-04-15 15:44:36 +0200

    drop arrow-specific code

--- R/cass_explorer_app_server.R
+++ R/cass_explorer_app_server.R
@@ -20,7 +20,8 @@ cass_explorer_app_server <- function(input, output, session) {
   output$date_range_input_UI <- renderUI({
     date_range_s <- golem::get_golem_options("corpus") %>%
       dplyr::distinct(date) %>%
-      dplyr::pull(date, as_vector = FALSE)
+      dplyr::collect() |> 
+      dplyr::pull(date)
 
     shiny::dateRangeInput(
       inputId = "date_range",
