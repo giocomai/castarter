4eee4587cb2d81474b0009469e670b255fb2ea5a
Giorgio Comai <g@giorgiocomai.eu>
2024-04-18 23:54:25 +0200

    update to consistent argument naming for patterns

--- R/cas_count_relative.R
+++ R/cas_count_relative.R
@@ -8,7 +8,7 @@
 #' @param full_words_only Defaults to FALSE. If FALSE, string is counted even when the it is found in the middle of a word (e.g. if FALSE, "ratio" would be counted as match in the word "irrational").
 #' @param locale Locale to be used when ignore_case is set to TRUE. Passed to `stringr::str_to_lower`, defaults to "en".
 #' @param n_column_name Defaults to 'n'. The unquoted name of the column to be used for the count in the output.
-#' @param string_column_name Defaults to 'word'. The unquoted name of the column to be used for the word in the output (if `include_string` is set to TRUE, as per default).
+#' @param pattern_column_name Defaults to 'word'. The unquoted name of the column to be used for the word in the output (if `include_string` is set to TRUE, as per default).
 #'
 #' @return A data frame
 #' @export
@@ -17,7 +17,7 @@
 #' \dontrun{
 #' cas_count_relative(
 #'   corpus = corpus,
-#'   string = c("dogs", "cats", "horses"),
+#'   pattern = c("dogs", "cats", "horses"),
 #'   text = text,
 #'   group_by = date,
 #'   n_column_name = n
@@ -31,7 +31,7 @@ cas_count_relative <- function(corpus,
                                ignore_case = TRUE,
                                fixed = FALSE,
                                full_words_only = FALSE,
-                               string_column_name = string,
+                               pattern_column_name = pattern,
                                n_column_name = n,
                                locale = "en") {
   total_words_df <- cas_count_total_words(
@@ -59,7 +59,7 @@ cas_count_relative <- function(corpus,
     ) %>%
     dplyr::transmute(
       {{ group_by }},
-      {{ string_column_name }},
+      {{ pattern_column_name }},
       {{ n_column_name }} := cas_n / cas_total
     )
 }
