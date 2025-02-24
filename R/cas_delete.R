59456dcf9dba7c053600392c9f3785619a4c5413
Giorgio Comai <g@giorgiocomai.eu>
2025-02-24 15:21:43 +0100

    allow for filtering by both id and batch

--- R/cas_delete.R
+++ R/cas_delete.R
@@ -3,7 +3,7 @@
 #' @param table Name of the table from where rows should be deleted.
 #' @param id Defaults to NULL. A vector of id. Rows with the given id will be
 #'   removed from the database.
-#' @param batch  Defaults to NULL. A vector of batch identigiers. Rows with the
+#' @param batch  Defaults to NULL. A vector of batch identifiers. Rows with the
 #'   given batch id will be removed from the database.
 #' @param index_group  Defaults to NULL. A vector of "index_group" names. Rows
 #'   with the given "index_group" will be removed from the database.
@@ -20,17 +20,27 @@
 #'   cas_delete_from_db(table = "contents_data", id = id_to_delete)
 #' }
 #' }
-cas_delete_from_db <- function(table,
-                               id = NULL,
-                               batch = NULL,
-                               index_group = NULL,
-                               ask = TRUE,
-                               db_folder = NULL,
-                               db_connection = NULL,
-                               disconnect_db = FALSE,
-                               ...) {
+cas_delete_from_db <- function(
+  table,
+  id = NULL,
+  batch = NULL,
+  index_group = NULL,
+  ask = TRUE,
+  db_folder = NULL,
+  db_connection = NULL,
+  disconnect_db = FALSE,
+  ...
+) {
   if (cas_check_use_db(...) == FALSE) {
-    cli::cli_abort("Database not set. Set the database connection with `cas_set_options()` or pass database connection with the parameter `db_connection`.")
+    cli::cli_abort(
+      "Database not set. Set the database connection with `cas_set_options()` or pass database connection with the parameter `db_connection`."
+    )
+  }
+
+  if (!is.null(index_group) & (!is.null(id) | !is.null(batch))) {
+    cli::cli_abort(
+      "Only removal of whole index groups supported at this point."
+    )
   }
 
   db <- cas_connect_to_db(
@@ -43,11 +53,17 @@ cas_delete_from_db <- function(table,
     cli::cli_abort(message = "Table {.var {table}} does not exist.")
   }
 
-  if (is.null(id) == FALSE) {
-    if (ask == TRUE) {
+  if (!is.null(id) & !is.null(batch)) {
+    if (length(batch) > 1 & length(id) > 1) {
+      cli::cli_abort(
+        message = "When providing both {.var id} and {.var batch} only one of them can be a vector of length higher than 1."
+      )
+    }
+    if (ask) {
       rows_to_be_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("SELECT id FROM {`table`} WHERE (id = {id})",
+        .x = glue::glue_sql(
+          "SELECT id, batch FROM {`table`} WHERE (id = {id}) AND (batch = {batch})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -60,7 +76,12 @@ cas_delete_from_db <- function(table,
           nrow(result)
         }
       )
-      confirm <- usethis::ui_yeah(x = glue::glue("A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"))
+
+      confirm <- usethis::ui_yeah(
+        x = glue::glue(
+          "A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"
+        )
+      )
     } else {
       confirm <- TRUE
     }
@@ -68,7 +89,53 @@ cas_delete_from_db <- function(table,
     if (isTRUE(confirm)) {
       rows_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("DELETE FROM {`table`} WHERE (id = {id})",
+        .x = glue::glue_sql(
+          "DELETE id, batch FROM {`table`} WHERE (id = {id}) AND (batch = {batch})",
+          .con = db
+        ),
+        .f = function(current_statement) {
+          result <- DBI::dbExecute(
+            conn = db,
+            statement = current_statement
+          )
+          result
+        }
+      )
+    } else {
+      return(invisible(NULL))
+    }
+  } else if (is.null(id) == FALSE) {
+    if (ask == TRUE) {
+      rows_to_be_deleted_v <- purrr::map_dbl(
+        .progress = TRUE,
+        .x = glue::glue_sql(
+          "SELECT id FROM {`table`} WHERE (id = {id})",
+          .con = db
+        ),
+        .f = function(current_statement) {
+          result <- DBI::dbGetQuery(
+            conn = db,
+            statement = current_statement
+          ) %>%
+            dplyr::collect()
+
+          nrow(result)
+        }
+      )
+      confirm <- usethis::ui_yeah(
+        x = glue::glue(
+          "A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"
+        )
+      )
+    } else {
+      confirm <- TRUE
+    }
+
+    if (isTRUE(confirm)) {
+      rows_deleted_v <- purrr::map_dbl(
+        .progress = TRUE,
+        .x = glue::glue_sql(
+          "DELETE FROM {`table`} WHERE (id = {id})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -87,7 +154,8 @@ cas_delete_from_db <- function(table,
     if (ask == TRUE) {
       rows_to_be_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("SELECT batch FROM {`table`} WHERE (batch = {batch})",
+        .x = glue::glue_sql(
+          "SELECT batch FROM {`table`} WHERE (batch = {batch})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -100,7 +168,11 @@ cas_delete_from_db <- function(table,
           nrow(result)
         }
       )
-      confirm <- usethis::ui_yeah(x = glue::glue("A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"))
+      confirm <- usethis::ui_yeah(
+        x = glue::glue(
+          "A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"
+        )
+      )
     } else {
       confirm <- TRUE
     }
@@ -108,7 +180,8 @@ cas_delete_from_db <- function(table,
     if (isTRUE(confirm)) {
       rows_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("DELETE FROM {`table`} WHERE (batch = {batch})",
+        .x = glue::glue_sql(
+          "DELETE FROM {`table`} WHERE (batch = {batch})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -127,7 +200,8 @@ cas_delete_from_db <- function(table,
     if (ask == TRUE) {
       rows_to_be_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("SELECT index_group FROM {`table`} WHERE (index_group = {index_group})",
+        .x = glue::glue_sql(
+          "SELECT index_group FROM {`table`} WHERE (index_group = {index_group})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -140,7 +214,11 @@ cas_delete_from_db <- function(table,
           nrow(result)
         }
       )
-      confirm <- usethis::ui_yeah(x = glue::glue("A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"))
+      confirm <- usethis::ui_yeah(
+        x = glue::glue(
+          "A total of {scales::number(sum(rows_to_be_deleted_v))} rows is going to be removed from the table {sQuote(table)}. Do you confirm?"
+        )
+      )
     } else {
       confirm <- TRUE
     }
@@ -148,7 +226,8 @@ cas_delete_from_db <- function(table,
     if (isTRUE(confirm)) {
       rows_deleted_v <- purrr::map_dbl(
         .progress = TRUE,
-        .x = glue::glue_sql("DELETE FROM {`table`} WHERE (index_group = {index_group})",
+        .x = glue::glue_sql(
+          "DELETE FROM {`table`} WHERE (index_group = {index_group})",
           .con = db
         ),
         .f = function(current_statement) {
@@ -170,5 +249,9 @@ cas_delete_from_db <- function(table,
     disconnect_db = disconnect_db
   )
 
-  cli::cli_inform(message = c(i = "{sum(rows_deleted_v)} rows have been deleted from the table {.var {table}}"))
+  cli::cli_inform(
+    message = c(
+      i = "{sum(rows_deleted_v)} rows have been deleted from the table {.var {table}}"
+    )
+  )
 }
