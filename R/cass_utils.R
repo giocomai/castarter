a8cffd081c0c936098bb33ce2d7580b52f481176
Giorgio Comai <g@giorgiocomai.eu>
2025-03-28 10:07:19 +0100

    fix utils documentation, including of non-exported function

--- R/cass_utils.R
+++ R/cass_utils.R
@@ -14,15 +14,15 @@ cass_split_string <- function(string,
                               squish = TRUE,
                               to_lower = TRUE,
                               to_regex = FALSE) {
-  v <- string %>%
+  v <- string |>
     stringr::str_split(
       pattern = ",",
       simplify = TRUE
-    ) %>%
+    ) |>
     as.character()
 
   if (squish == TRUE) {
-    v <- v %>%
+    v <- v |>
       stringr::str_squish()
   }
 
@@ -32,8 +32,8 @@ cass_split_string <- function(string,
   }
 
   if (to_regex == TRUE) {
-    v <- v %>%
-      stringr::str_c(collapse = "|")
+    v <- v |>
+      stringr::str_flatten(collapse = "|")
   }
   v
 }
@@ -42,15 +42,16 @@ cass_split_string <- function(string,
 #' Combines a vector of words into a string to be used for regex matching.
 #'
 #' @param words A character vector of words to be combined for string matching.
-#' @param full_words_only Logical, defaults to TRUE. If TRUE, the correspondent words are matched only when they are a separate word.
+#' @param full_words_only Logical, defaults to TRUE. If TRUE, the correspondent
+#'   words are matched only when they are a separate word.
 #'
-#' @return A character vector of length one, ready to be used for regex matching.
+#' @return A character vector of length one, ready to be used for regex
+#'   matching.
 #'
 #' @examples
-#'
 #' words <- c("dogs", "cats", "horses")
 #'
-#' cass_combine_into_pattern(words)
+#' castarter:::cass_combine_into_pattern(words)
 cass_combine_into_pattern <- function(words,
                                       full_words_only = TRUE) {
   if (full_words_only == TRUE) {
@@ -63,11 +64,11 @@ cass_combine_into_pattern <- function(words,
           "\\b"
         )
       }
-    ) %>%
-      stringr::str_c(collapse = "|")
+    ) |>
+      stringr::str_flatten(collapse = "|")
   } else {
-    pattern <- words %>%
-      stringr::str_c(collapse = "|")
+    pattern <- words |>
+      stringr::str_flatten(collapse = "|")
   }
   pattern
 }
@@ -75,14 +76,34 @@ cass_combine_into_pattern <- function(words,
 
 #' Get base folder under which files will be stored.
 #'
-#' @param level Defaults to "website". Valid values are "website", "project", and "base".
-#' @param custom_path Defaults to NULL. If given, all other parameters and settings are ignored, and folder is set to this value.
-#' @param ... Passed to `cas_get_options()`.
+#' The path is computed based on inputs and settings; it may or may not exist.
 #'
-#' @return
+#' @param level Defaults to "website". Valid values are "website", "project",
+#'   and "base".
+#' @param custom_path Defaults to NULL. If given, all other parameters and
+#'   settings are ignored, and this value is returned.
+#' @param ... Passed to [cas_get_options()].
+#'
+#' @return Path to the folder well all project files are stored.
 #' @export
 #'
 #' @examples
+#'
+#'
+#' cas_set_options(
+#'   base_folder = fs::path(tempdir(), "R", "castarter_data"),
+#'   db_folder = fs::path(tempdir(), "R", "castarter_data"),
+#'   project = "example_project",
+#'   website = "example_website"
+#' )
+#'
+#' cas_get_base_folder()
+#'
+#' cas_get_base_folder(level = "website")
+#' cas_get_base_folder(level = "project")
+#' cas_get_base_folder(level = "base")
+#'
+#' cas_get_base_folder(custom_path = fs::path(tempdir(), "custom_path"))
 cas_get_base_folder <- function(...,
                                 level = "website",
                                 custom_path = NULL) {
