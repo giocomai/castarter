16848f047fe09a8ec2379e52dbfbb419a201e72f
Giorgio Comai <g@giorgiocomai.eu>
2025-03-29 13:33:41 +0100

    proper documentation for cas_get_sitemap

--- R/cas_get_sitemap.R
+++ R/cas_get_sitemap.R
@@ -1,26 +1,51 @@
-#' Check for availability of sitemap.xml
+#' Checks for availability of a sitemap in xml format.
 #'
-#' Searches in default location and then in robots.txt and return url
+#' Searches in common locations (namely, `example.com/sitemap.xml`, and
+#' `example.com/sitemap_index.xml`) and then in robots.txt and returns a url to
+#' the sitemap, along with the contents of the sitemap itself, if found.
 #'
-#' @param domain
-#' @param sitemap_url
+#' @param domain Defaults to `NULL`, but required unless `sitemap_url` given.
+#'   Expected to be a full domain name. If input does not start with `http`,
+#'   then `https://` is prepended automatically.
+#' @param sitemap_url Defaults to `NULL`. If given, `domain` is ignored.
 #'
 #' @returns A data frame, including a `sitemap_url` column, the response as an
-#'   httr2 object, and body of the xml.
+#'   httr2 object, and the body of the xml.
 #' @export
 #'
 #' @examples
 #' if (interactive()) {
-#' cas_get_sitemap("https://www.europeandatajournalism.eu/")
+#'   cas_get_sitemap("https://www.europeandatajournalism.eu/")
 #' }
 cas_get_sitemap <- function(
-  domain,
+  domain = NULL,
   sitemap_url = NULL
 ) {
   sitemap_response <- NULL
 
+  if (stringr::str_starts(string = domain, pattern = "http", negate = TRUE)) {
+    domain <- stringr::str_flatten(string = c("https://", domain))
+  }
+
   if (is.null(sitemap_url)) {
     url <- httr2::url_modify(url = domain, path = "sitemap.xml")
+
+    req <- httr2::request(url) |>
+      httr2::req_error(is_error = \(resp) FALSE)
+
+    resp <- tryCatch(req |> httr2::req_perform(), error = \(e) FALSE)
+
+    if (isFALSE(resp)) {
+      sitemap_url <- NULL
+    } else if (!httr2::resp_has_body(resp)) {
+      sitemap_url <- NULL
+    } else {
+      sitemap_response <- resp
+    }
+  }
+
+  if (is.null(sitemap_url)) {
+    url <- httr2::url_modify(url = domain, path = "sitemap_index.xml")
 
     req <- httr2::request(url) |>
       httr2::req_error(is_error = \(resp) FALSE)
